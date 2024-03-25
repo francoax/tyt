bc965a20752b5e375e877bc55f5c0dbd34a28860
francoax <francoa.duarte2001@gmail.com>
2024-03-25 10:50:57 -0300

    content security fixed.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
@@ -12,10 +26,6 @@ const nextConfig = {
           {
             key: 'X-XSS-Protection',
             value: '1; mode=block',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'",
           },
           {
             key: 'X-Content-Type-Options',
@@ -40,6 +50,10 @@ const nextConfig = {
           {
             key: 'X-DNS-Prefetch-Control',
             value: 'on'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
           },
         ]
       }
