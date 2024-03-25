7a452ee41867f74f1dab71b7f6b9608f040391a2
francoax <francoa.duarte2001@gmail.com>
2024-03-25 10:17:57 -0300

    headers setted.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,52 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    const headers = []
+
+    if(isProduction) headers.push(
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          }
+        ]
+      }
+    )
+
+    return headers
+  }
+};
 
 module.exports = nextConfig;
